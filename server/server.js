e7dc69ae7137ea1c9b3ee80af57a2822ec2eb0a6
AnuragMishra9341 <anurag102005@gmail.com>
2026-01-27 00:03:11 +0530

    Add server folder

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors';
+import 'dotenv/config';
+import { clerkMiddleware, clerkClient, requireAuth, getAuth } from '@clerk/express'
+import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './config/cloudinary.js';
+/* 1>clerkMiddleware parses the req and attaches and auth object to it
+   2> required auth is used a route protector 
+*/
+
+const app = express();
+await connectCloudinary();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get('/',(req,res)=>{
+    res.send('Server is live!');
+})  // public route
+
+app.use(requireAuth());
+
+// routes  all are protected 
+
+app.use('/api/ai',aiRouter);
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log('Server is running on port',PORT)
+})
